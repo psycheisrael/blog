a0f0461de25ddc9bc77843ef1a3c5f5180ec68eb
psycheisrael <mudyaisrael@gmail.com>
2020-04-28 18:39:26 -0400

    commit to git

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+    #get '/comments', to: 'comments#index', as: 'comments'
+    #post '/comments', to: 'comments#create'
+    #get '/comments/new', to: 'comments#new', as: 'new_comments'
+    #get '/comments/:id', to: 'comments#show', as: 'comment'
+    #get '/comments/:id/edit', to: 'comments#edit', as: 'edit_comments'
+    #patch '/comments/:id', to: 'comments#update'
+    #delete '/comments/:id', to: 'comments#destroy'
+  end
+
   resources :pages
 #  get '/pages', to: 'pages#index'
 #  post '/pages', to: 'pages#create'
